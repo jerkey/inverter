62f68ff40272dce711b2d11e021140cb46354e01
jake waters <jerkey@g-cipher.net>
2017-02-01 18:49:57 -0800

    choose output pins and start setting them up

--- inverter.ino
+++ inverter.ino
@@ -1,8 +1,13 @@
-#include "sinewave.h"
+#include "sinewave.h" // sinewave_length=256  sinewave_data[256]
 #define SAMPLE_RATE 15360// 256*60
-#define OUT1    13
+#define L1_low  9 // OC1A PB1
+#define L1_high 8 // PB0
+#define L2_low  10// OC1B PB2
+#define L2_high 12// PB4
+
 unsigned long lastTime = millis();
 volatile uint32_t sample = 0;
+volatile byte phase = 0; // 0 = L1 is HIGH, 1 = L2 is HIGH
 // This is called at SAMPLE_RATE kHz to load the next sample.
 ISR(TIMER2_COMPA_vect) {
   sample+=1;
@@ -22,6 +27,13 @@ void setup30720hzInterrupt() {
   TIMSK2 |= _BV(OCIE2A);
   sei(); // enable interrupts
 }
+
+void setupPWMs() {
+  pinMode(L1_low,OUTPUT);
+  pinMode(L1_high,OUTPUT);
+  pinMode(L2_low,OUTPUT);
+  pinMode(L2_high,OUTPUT);
+  // timer1 pins 9 and 10
 
 void setup() {
   Serial.begin(57600);
